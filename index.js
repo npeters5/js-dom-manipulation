fc86b8dae4e68a31960b462b7da70c58a4dc5fb0
Nora Peters <norapeters5@gmail.com>
2017-09-25 16:39:36 -0700

    event listeners scroll, focus, blur

--- index.js
+++
@@ -1,8 +0,0 @@
-const iceCream = document.querySelector(".ice-cream");
-const parent = document.querySelector(".parent");
-const link = document.querys(".link");
-
-//two different ways to write the same thing - arrow vs normal(?)
-
-iceCream.addEventListener("click", event => console.log(event));
-// iceCream.addEventListener('click', function(event) {} );
